ed35ffc49d8b34737746e47563a0c250187bd4f7
scarangelloNBC <anthony.scarangello@nbcuni.com>
2019-11-26 12:30:11 -0500

    Added inning support, adjusted some thresholds

--- src/main/scala/ComeOnBlueApp.scala
+++ src/main/scala/ComeOnBlueApp.scala
@@ -1,6 +1,9 @@
 import java.time.LocalDate
 
 object ComeOnBlueApp extends App {
-  val allGamesForDay = GameUtil.getAllGames(LocalDate.parse("2019-05-20"))
+    val allGamesForDay = GameUtil.getAllGames(LocalDate.parse("2019-10-30")).sortWith {
+    case (gameInfo1: GameInfo, gameInfo2: GameInfo) =>
+      gameInfo1.pitches.length > gameInfo2.pitches.length
+  }
   println(allGamesForDay.map(_.stringFormat).mkString(""))
 }
--- src/main/scala/PitchUtil.scala
+++ src/main/scala/PitchUtil.scala
@@ -17,7 +17,7 @@ object PitchUtil {
   def verticalCheck(topLimit: Double, botLimit: Double, pz: Double, description: String): Boolean = {
     description match {
       case "Called Strike" =>
-        pz > (topLimit + (ballDiameter * 2.5)) || pz < (botLimit - (ballDiameter * 2.5))
+        pz > (topLimit + (ballDiameter * 2.0)) || pz < (botLimit - (ballDiameter * 2.0))
       case "Ball" =>
         pz < (topLimit - (ballDiameter * 1.25)) && pz > (botLimit + (ballDiameter * 1.25))
     }
@@ -34,23 +34,27 @@ object PitchUtil {
 
   def getAllPitchesFromGame(xmlDoc: Elem): (Seq[Option[PitchInfo]]) = {
     try {
-      val allAtBats = xmlDoc \\ "atbat"
-      allAtBats.flatMap {
-        case atBat: Node =>
-          val pitches = atBat \\ "pitch"
-          val playResult = (atBat \ "@des").toString
-          pitches.map {
-            case pitch: Node =>
-              if((pitch \ "@des").nonEmpty && (pitch \ "@sz_top").nonEmpty && (pitch \ "@sz_bot").nonEmpty && (pitch \ "@px").nonEmpty && (pitch \ "@pz").nonEmpty &&
-                ((pitch \ "@des").toString == "Ball" || (pitch \ "@des").toString == "Called Strike")) {
-                Some(PitchInfo((pitch \\ "@des").toString, (pitch \\ "@sz_top").toString.toDouble, (pitch \\ "@sz_bot").toString.toDouble, (pitch \ "@px").toString.toDouble, (pitch \ "@pz").toString.toDouble, playResult))
-              }
-              else {
-                None
+      val allInnings = xmlDoc \\ "inning"
+      allInnings.flatMap {
+        case inning: Node =>
+          val inningNum = (inning \ "@num").toString.toInt
+          val allAtBats = inning \\ "atbat"
+          allAtBats.flatMap {
+            case atBat: Node =>
+              val pitches = atBat \\ "pitch"
+              val playResult = (atBat \ "@des").toString
+              pitches.map {
+                case pitch: Node =>
+                  if((pitch \ "@des").nonEmpty && (pitch \ "@sz_top").nonEmpty && (pitch \ "@sz_bot").nonEmpty && (pitch \ "@px").nonEmpty && (pitch \ "@pz").nonEmpty &&
+                    ((pitch \ "@des").toString == "Ball" || (pitch \ "@des").toString == "Called Strike")) {
+                    Some(PitchInfo((pitch \\ "@des").toString, (pitch \\ "@sz_top").toString.toDouble, (pitch \\ "@sz_bot").toString.toDouble, (pitch \ "@px").toString.toDouble, (pitch \ "@pz").toString.toDouble, inningNum, playResult))
+                  }
+                  else {
+                    None
+                  }
               }
           }
       }
-
     }
     catch {
       case exception: Throwable =>
